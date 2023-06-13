6a7bab45fb2fd0d82a26d265fa823621da18df59
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2023-06-13 19:26:13 +0530

    done
    uploaded final file

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-final-matic-snow-matic-steel--matic-hl-1origrgb",
+      "name": "FINAL MATIC SNOW MATIC STEEL  MATIC HL-1.origRGB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.056914574513062,
+        "pitch": 0,
+        "fov": 1.4461899673881597
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-final-kitchen-1-1--strip-sparkle-black-strip-sparkle-whiteorigrgb",
+      "name": "FINAL KITCHEN 1 1  STRIP SPARKLE BLACK STRIP SPARKLE WHITE.origRGB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
